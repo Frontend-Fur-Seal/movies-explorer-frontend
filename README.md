3754e5bebb03a7c032517a967cb3a0145a2abb08
Vladimir Sherstnev <voralbergann@gmail.com>
2023-08-08 13:17:47 +0300

    newbranch

--- README.md
+++
@@ -1,5 +0,0 @@
-# Frontend дипломного проекта MoviesExplorer
-
-### Макет: Dark-2: https://www.figma.com/file/vxZYy7uSJECy3jn4BOvrDm/dark-2?type=design&mode=design&t=eTOZZsSWvGz4XNWd-1
-
-
--- src/components/UserGreeting/UserGreeting.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-function UserGreeting(props) {
-    return (
-        <button className={props.variable}><Link to="/profile" className="header__account-link">Аккаунт</Link></button>
-    );
-  }
-  
-  export default UserGreeting;
