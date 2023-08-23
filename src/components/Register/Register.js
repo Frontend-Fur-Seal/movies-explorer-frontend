b450c68ad7618e3cc53e3a11f87fe62fb9086832
Vladimir Sherstnev <voralbergann@gmail.com>
2023-08-23 19:50:31 +0300

    validateRegister

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,29 +1,41 @@
-import { useState } from "react";
 import FormData from "../FormData/FormData.js";
+import { Formik, Form, Field } from "formik";
 
-function Register( {handleRegister} ) {
+function Register({ handleRegister }) {
+  function validateName(value) {
+    let error;
+    if (!value) {
+      error = "Поле обязательно к заполнению";
+    } else if (value.length < 2) {
+      error = "Слишком короткое имя";
+    } else if (value.length > 30) {
+      error = "Имя слишком длинное";
+    }
+    return error;
+  }
 
-  const [formValue, setFormValue] = useState({
-    name: '',
-    email: '',
-    password: ''
-  })
-  
-const handleChange = (e) => {
-  const {name, value} = e.target;
+  function validateEmail(value) {
+    let error;
+    if (!value) {
+      error = "Поле обязательно к заполнению";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+      error = "Некорректный email";
+    }
+    return error;
+  }
 
-  setFormValue({
-    ...formValue,
-    [name]: value
-  });
-}
+  function validatePassword(value) {
+    let error;
+    if (!value) {
+      error = "Поле обязательно к заполнению";
+    } else if (value.length < 2) {
+      error = "Слишком короткий пароль";
+    } else if (value.length > 30) {
+      error = "Пароль слишком длинный";
+    }
+    return error;
+  }
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    handleRegister(formValue.name, formValue.email, formValue.password)
-}
-
-console.log(formValue)
   return (
     <main className="main">
       <FormData
@@ -32,56 +44,74 @@ console.log(formValue)
         changeLink={"Войти"}
         link={"/signin"}
       >
-        <form className="register" onSubmit={handleSubmit}>
-          <label htmlFor="registerInput-name" className="form__label">
-            Имя
-          </label>
-          <input
-            placeholder="Имя"
-            id="registerInput-name"
-            required={true}
-            minLength={2}
-            maxLength={20}
-            type="text"
-            className="form__input"
-            onChange={handleChange}
-            value={formValue.name}
-            name="name"
-          />
-          <p className="form__error"></p>
-          <label htmlFor="registerInput-email" className="form__label">
-            E-mail
-          </label>
-          <input
-            placeholder="E-mail"
-            id="registerInput-email"
-            required={true}
-            type="email"
-            className="form__input"
-            onChange={handleChange}
-            value={formValue.email}
-            name="email"
-            autoComplete='on'
-          />
-          <p className="form__error"></p>
-          <label htmlFor="registerInput-password" className="form__label">
-            Пароль
-          </label>
-          <input
-            placeholder="Пароль"
-            id="registerInput-password"
-            required={true}
-            minLength={2}
-            maxLength={20}
-            type="password"
-            className="form__input"
-            onChange={handleChange}
-            value={formValue.password}
-            name="password"
-          />
-          <p className="form__error"></p>
-          <button type="submit" className='form__submit-register'>Зарегистрироваться</button>
-        </form>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          onSubmit={(values) => {
+            handleRegister(values.name, values.email, values.password);
+          }}
+        >
+          {({ errors, touched, isValid, dirty }) => (
+            <Form className="register">
+              <label htmlFor="registerInput-name" className="form__label">
+                Имя
+              </label>
+              <Field
+                validate={validateName}
+                placeholder="Имя"
+                id="registerInput-name"
+                type="text"
+                className="form__input"
+                name="name"
+              />
+              {errors.name && touched.name && (
+                <p className="form__error">{errors.name}</p>
+              )}
+              <label htmlFor="registerInput-email" className="form__label">
+                E-mail
+              </label>
+              <Field
+                validate={validateEmail}
+                placeholder="E-mail"
+                id="registerInput-email"
+                type="email"
+                className="form__input"
+                name="email"
+                autoComplete="on"
+              />
+              {errors.email && touched.email && (
+                <p className="form__error">{errors.email}</p>
+              )}
+              <label htmlFor="registerInput-password" className="form__label">
+                Пароль
+              </label>
+              <Field
+                validate={validatePassword}
+                placeholder="Пароль"
+                id="registerInput-password"
+                type="password"
+                className="form__input"
+                name="password"
+              />
+              {errors.password && touched.password && (
+                <p className="form__error">{errors.password}</p>
+              )}
+              <button
+                type="submit"
+                disabled={
+                  !isValid ||
+                  (Object.keys(touched).length === 0 &&
+                    touched.constructor === Object)
+                }
+                className="form__submit-register"
+              >
+                Зарегистрироваться
+              </button>
+            </Form>
+          )}
+        </Formik>
       </FormData>
     </main>
   );
