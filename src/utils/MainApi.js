68eda6dc974395d9fa2d1f8b1adf6bd13fd8a90f
Vladimir Sherstnev <voralbergann@gmail.com>
2023-08-25 16:36:59 +0300

    api

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -68,7 +68,7 @@ class MainApi {
   }
   
   const mainApi = new MainApi({
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://api.diploma.sherstnev.nomoredomains.xyz",
     headers: {
       "Content-Type": "application/json",
     },
